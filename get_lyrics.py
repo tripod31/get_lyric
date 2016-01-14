bad9d53a27f3a553d2562a92d4d278d5ae226813
tripod31 <tripod31@hotmail.com>
2016-01-14 21:12:13 +0900

    upd

--- get_lyrics.py
+++ get_lyrics.py
@@ -9,19 +9,32 @@ output filename is "artist - song.txt".
 import argparse
 import logging
 from get_lyric.www_lyrics_az import www_lyrics_az
+from get_lyric.j_lyric_net import j_lyric_net
 import io,os
 import stagger
+import re
+from email.policy import default
 
 args = None
 
+def is_all_ascii(s):
+    re1 = re.compile(r'^[\x20-\x7E]+$')
+    if re1.search(s) is None:
+        return False
+    else:
+        return True
+        
 def find_all_files(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             yield os.path.join(root, file)
 
 def get_lyric(artist,song,buf):
-    scrapers = [www_lyrics_az(artist,song)] 
-    
+    if is_all_ascii(artist) and is_all_ascii(song):
+        scrapers = [www_lyrics_az(artist,song)]
+    else:
+        scrapers = [j_lyric_net(artist,song)]
+        
     for scraper in scrapers:
         try:
             ret=scraper.get_lyric()
@@ -46,6 +59,14 @@ def process_mp3(file):
     else:
         print("found")
     lyric = buf.getvalue()
+    '''
+    if args.write2tag:
+        pass
+    '''    
+    if "out_dir" in vars(args):
+        write2file(artist, song, lyric)
+
+def write2file(artist,song,lyric):
     filename = "%s - %s.txt" % (artist,song)
     path = os.path.join(args.out_dir,filename)
     try:
@@ -60,6 +81,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--in_dir')
     parser.add_argument('--out_dir')
+    #parser.add_argument('--write2tag'   ,action='store_true')
     
     args=parser.parse_args()
     logging.basicConfig(level=logging.INFO,
